31317b7497fb282cb58c8e14c0fae9896dfba7a2
5S <7006470+5S@users.noreply.github.com>
2024-11-06 17:30:54 +0900

    Add privacy and terms

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,11 +2,17 @@ export default function Footer() {
   return (
     <footer className="flex flex-col items-center gap-2 py-6 text-[#9198a1]">
       <p>© 2024 HCl.</p>
-      <ul>
+      <ul className="flex items-center gap-4">
         <li>
           <a href="/">Home</a>
         </li>
-        <li className="mx-2">
+        <li>
+          <a href="/privacy">プライバシーポリシー</a>
+        </li>
+        <li>
+          <a href="/terms">利用規約</a>
+        </li>
+        <li>
           <a href="/commerce">特定商取引法に基づく表記</a>
         </li>
       </ul>
